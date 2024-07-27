8b3a3d28fead5236c3912dbfe7c9c4ac6dfccaa3
kurt andrei gutierrez <kurtandrei50@gmail.com>
2024-07-27 23:50:58 +0800

    event preventdefaultpractice

--- form/form.js
+++ form/form.js
@@ -1,5 +1,23 @@
-const form = document.querySelector("#shelter");
+const form = document.querySelector("#k");
+const input1 = document.querySelector("#shelter");
+const input2 = document.querySelector("#shelter2");
+const list = document.querySelector("#list");
+form.addEventListener("submit", (event) =>{
 
-form.addEventListener("submit", function(){
-    
+    event.preventDefault();
+    // const input = input1.value;
+    // const input3 = input2.value;
+
+    yown(input1, input2);
+ 
+})  
+
+const yown = ((input1, input2) =>{
+    const li = document.createElement("li");
+    li.innerText = `${input1.value} ${input2.value}` ;
+    list.append(li);    
+    console.log(li);
+
+    input1.value = "";
+    input2.value = "";
 })
